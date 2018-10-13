493d2ce38562781ff4570093e2e11e65e524a974
drewd39 <drewrdavis39@gmail.com>
2018-10-13 07:04:18 +0000

    ERG Table populated

--- server/dataTables/models.py
+++ server/dataTables/models.py
@@ -9,7 +9,20 @@ class Resource(models.Model):
     company_name = models.CharField(max_length=50)
     author_name  = models.CharField(max_length=50)
 
+
 class Profile(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     company = models.CharField(max_length=50)
+
+class ERG(models.Model):
+    manual_id = models.IntegerField()
+    contact_name = models.CharField(max_length=50)
+    contact_email = models.CharField(max_length=50)
+    company_name = models.CharField(max_length=50)
+    city_name = models.CharField(max_length=50)
+    industry = models.CharField(max_length=50)
+    #post_date = models.DateField()
+    post_date = models.CharField(max_length=50)
+    identification = models.CharField(max_length=50)
+
